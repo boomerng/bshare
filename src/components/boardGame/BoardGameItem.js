fb047d425a52681320d7884385d5b93dd200aa7f
Xin Chen <roger@Xins-MacBook-Air.local>
2019-09-22 17:35:01 -0400

    gameList

--- src/components/boardGame/BoardGameItem.js
+++ src/components/boardGame/BoardGameItem.js
@@ -1,9 +1,33 @@
 import React from 'react';
 import classNames from 'classnames';
+import { makeStyles } from '@material-ui/core/styles';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import InputLabel from '@material-ui/core/InputLabel';
+import Avatar from '@material-ui/core/Avatar';
+import Divider from '@material-ui/core/Divider';
 
-import '../../styles/boardGame/BoardGameItem.css';
+// import '../../styles/boardGame/BoardGameItem.css';
+const useStyles = makeStyles(() => ({
+  left: {
+    minWidth: 140
+  },
+  avatar: {
+    width: 120,
+    height: 120
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 600
+  },
+  label: {
+    display: 'block'
+  }
+}));
 
 const BoardGameItem = (props) => {
+  const classes = useStyles();
+
   const {
     name,
     thumbnail,
@@ -13,23 +37,20 @@ const BoardGameItem = (props) => {
     isExpansion
   } = props;
   return (
-    <div className={classNames(`container`, `rowDirection`)}>
-      <img src={thumbnail} alt="image not available" className={classNames(`thumbnail`)} /> 
-      <div className={classNames(`container`, `colDirection`)}>
-        <div className={`title`}>
-          {name}
-        </div>
+    <React.Fragment>
+      <ListItem>
+        <ListItemAvatar className={classes.left}>
+          <Avatar className={classes.avatar} src={thumbnail} alt="image not available" />
+        </ListItemAvatar>
         <div>
-          {`Players:${minPlayers}-${maxPlayers}`}
+          <InputLabel className={classNames(classes.title, classes.label)}>{name}</InputLabel>
+          <InputLabel className={classes.label}>{`Players:${minPlayers}-${maxPlayers}`}</InputLabel>
+          <InputLabel className={classes.label}>{`PlayingTime:${playingTime}`}</InputLabel>
+          <InputLabel className={classes.label}>{`Expansion: ${isExpansion ? 'Yes': 'No'}`}</InputLabel>
         </div>
-        <div>
-          {`PlayingTime:${playingTime}`}
-        </div>
-        <div>
-          {`Expansion: ${isExpansion ? 'Yes': 'No'}`}
-        </div>
-      </div>
-    </div>
+      </ListItem>
+      <Divider variant="inset" component="li"></Divider>
+    </React.Fragment>
   );
 };
 
--- src/components/boardGame/BoardGameList.js
+++ src/components/boardGame/BoardGameList.js
@@ -2,15 +2,28 @@ import React from 'react';
 import { getBoardGames } from 'src/api/DataApi';
 import BoardGameItem from './BoardGameItem';
 
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+    maxWidth: 480,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
 const BoardGameList = () => {
+  const classes = useStyles();
+
   return (
-    <div>
+    <List className={classes.root}>
       {
         getBoardGames().map((game) => (
           <BoardGameItem key={game.gameId} {...game} />
         ))
       }
-    </div>
+    </List>
   );
 };
 
